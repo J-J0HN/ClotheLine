67f19c73a9b2259affb0a5cd9579c290c1341d03
khalid osemi <osemikhalid193@gmail.com>
2023-04-05 01:11:56 +0100

    commit

--- public/Register.php
+++ public/Register.php
@@ -27,36 +27,16 @@ if (isset($_POST['submit'])) {
     $_SESSION['username'] = $user['username'];
 }
 
-$output = '
-<div class="title-container">
-  <h1>Account Details</h1>
-</div>
-<div class="form-container">
-  <form action="" method="POST">
+// Load the register HTML template
+ob_start();
+require '../templates/register.html.php';
+$output = ob_get_clean();
 
-  <label for="Firstname">Firstname:</label>
-    <input type="text" name="Firstname" id="Firstname" placeholder="Enter your Firstname" required>
 
-    <label for="Lastname">Lastname:</label>
-    <input type="text" name="Lastname" id="Lastname" placeholder="Enter your Lastname" required>
-    
-    <label for="username">Username:</label>
-    <input type="text" name="username" id="username" placeholder="Enter your username" required>
-
-    <label for="email">Email:</label>
-    <input type="email" name="email" id="email" placeholder="Enter your email" required>
-
-    <label for="password">Password:</label>
-    <input type="password" name="password" id="password" placeholder="Enter your password" required>
-
-    <button type="submit" name="submit" class="register-btn">Register</button>
-  </form>
-</div>';
-
-// check if user has registered and display link if true
 if (isset($user_registered)) {
     $output .= '<p>You have registered!</p>';
     $output .= '<a href="login.php" class="back-home">Go To Login</a>';
 }
+
 
 require '../templates/mainTemp.html.php';
---
+++ templates/register.html.php
@@ -0,0 +1,24 @@
+<div class="title-container">
+  <h1>Account Details</h1>
+</div>
+<div class="form-container">
+  <form action="" method="POST">
+
+  <label for="Firstname">Firstname:</label>
+    <input type="text" name="Firstname" id="Firstname" placeholder="Enter your Firstname" required>
+
+    <label for="Lastname">Lastname:</label>
+    <input type="text" name="Lastname" id="Lastname" placeholder="Enter your Lastname" required>
+    
+    <label for="username">Username:</label>
+    <input type="text" name="username" id="username" placeholder="Enter your username" required>
+
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" placeholder="Enter your email" required>
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password" placeholder="Enter your password" required>
+
+    <button type="submit" name="submit" class="register-btn">Register</button>
+  </form>
+</div>
