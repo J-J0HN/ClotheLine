9871dcae8616ec0bc69d6a91eb66577ac351231f
khalid osemi <osemikhalid193@gmail.com>
2023-04-17 14:36:53 +0100

    chagned

---
+++ templates/login.html.php
@@ -0,0 +1,15 @@
+<div class="title-container">
+  <h1>Login</h1>
+</div>
+<div class="form-container">
+  <form action="" method="POST">
+
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" placeholder="Enter your email" required>
+
+    <label for="password">Password:</label>
+    <input type="password" name="password" id="password" placeholder="Enter your password" required>
+
+    <button type="submit" name="submit" class="login-btn">Login</button>
+  </form>
+</div>
