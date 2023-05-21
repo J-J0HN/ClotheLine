e3e501105bb9cbd26c6a96343b59f67e35e6e69f
khalid osemi <osemikhalid193@gmail.com>
2023-05-21 19:22:58 +0100

    commit

--- templates/Account.html.php
+++ templates/Account.html.php
@@ -1,6 +1,5 @@
 <div class="container-user">
   <button class="button"><a href="editAccount.php">Manage Account</a></button>
-  <button class="button"><a href="ViewOrders.php">View orders</a></button>
   <button class="button"><a href="ManageCard.php">Manage debit/credit card</a></button>
 </div>
 
