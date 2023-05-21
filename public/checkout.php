933cc6c7ee5b5b8c16b7dcaf45fbfd9e3e7386c5
Joyal <64101875+J0YALJ0HN@users.noreply.github.com>
2023-05-21 18:29:39 +0100

    checkout

---
+++ public/checkout.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+require '../pdo.php';
+require '../functions.php';
+
+if (!isset($_SESSION['login'])) {
+    // Redirect the user to the login page if not logged in
+    header('Location: login.php');
+    exit();
+}
+
+if (isset($_POST['delivery_add_street']) && isset($_POST['delivery_add_county']) && isset($_POST['delivery_add_postcode']) && isset($_POST['delivery_add_country']) && isset($_POST['card_name']) && isset($_POST['card_number']) && isset($_POST['card_exp_date']) && isset($_POST['card_cvv']) && isset($_POST['phone_number'])) {
+    $delivery_add_street = $_POST['delivery_add_street'];
+    $delivery_add_county = $_POST['delivery_add_county'];
+    $delivery_add_postcode = $_POST['delivery_add_postcode'];
+    $delivery_add_country = $_POST['delivery_add_country'];
+    $card_name = $_POST['card_name'];
+    $card_number = $_POST['card_number'];
+    $card_exp_date = $_POST['card_exp_date'];
+    $card_cvv = $_POST['card_cvv'];
+    $phone_number = $_POST['phone_number'];
+
+    // Get user ID from session
+    $userid = $_SESSION['userid'];
+
+    // Insert order details into the order table
+    $query = "INSERT INTO `order` (prodid, delivery_add_street, delivery_add_county, delivery_add_postcode, delivery_add_country, price, quantity, card_name, card_number, card_exp_date, card_cvv, phone_number, billing_add_house_number, billing_add_street, billing_add_county, billing_add_postcode, billing_add_country, delivery_add_house_number, userid) VALUES ";
+
+    foreach ($_SESSION['bag'] as $item) {
+        $prodid = $item['prodid'];
+        $price = $item['price'];
+        $quantity = $item['quantity'];
+        $billing_add_house_number = $item['billing_add_house_number'];
+        $billing_add_street = $item['billing_add_street'];
+        $billing_add_county = $item['billing_add_county'];
+        $billing_add_postcode = $item['billing_add_postcode'];
+        $billing_add_country = $item['billing_add_country'];
+        $delivery_add_house_number = $item['delivery_add_house_number'];
+
+        $query .= "('$prodid', '$delivery_add_street', '$delivery_add_county', '$delivery_add_postcode', '$delivery_add_country', '$price', '$quantity', '$card_name', '$card_number', '$card_exp_date', '$card_cvv', '$phone_number', '$billing_add_house_number', '$billing_add_street', '$billing_add_county', '$billing_add_postcode', '$billing_add_country', '$delivery_add_house_number', '$userid'),";
+    }
+
+    // Remove the trailing comma from the query
+    $query = rtrim($query, ',');
+
+    $result = $pdo->query($query);
+
+    if ($result) {
+        // Order placed successfully
+        // Clear the session bag
+        $_SESSION['bag'] = [];
+
+        // Redirect to the order confirmation page
+        header('Location: order_confirmation.php');
+        exit();
+    } else {
+        // Error occurred while inserting the order details
+        echo "An error occurred while placing the order.";
+    }
+} else {
+    // Invalid form submission
+    header('Location: checkout.html.php');
+    exit();
+}
---
+++ templates/checkout.html.php
@@ -0,0 +1,32 @@
+<section class="checkout-page">
+    <h2 class="checkout-title">Checkout</h2>
+    <hr>
+    <form action="checkout.php" method="post" class="checkout-form">
+        <section class="delivery-details">
+            <h3 class="section-title">Delivery Details</h3>
+            <label for="delivery_add_street">Street:</label>
+            <input type="text" name="delivery_add_street" id="delivery_add_street" required>
+            <label for="delivery_add_county">County:</label>
+            <input type="text" name="delivery_add_county" id="delivery_add_county" required>
+            <label for="delivery_add_postcode">Postcode:</label>
+            <input type="text" name="delivery_add_postcode" id="delivery_add_postcode" required>
+            <label for="delivery_add_country">Country:</label>
+            <input type="text" name="delivery_add_country" id="delivery_add_country" required>
+            <hr>
+            <h3 class="section-title">Payment Details</h3>
+            <label for="card_name">Cardholder Name:</label>
+            <input type="text" name="card_name" id="card_name" required>
+            <label for="card_number">Card Number:</label>
+            <input type="text" name="card_number" id="card_number" required>
+            <label for="card_exp_date">Card Expiration Date:</label>
+            <input type="text" name="card_exp_date" id="card_exp_date" required>
+            <label for="card_cvv">Card CVV:</label>
+            <input type="text" name="card_cvv" id="card_cvv" required>
+            <hr>
+            <h3 class="section-title">Contact Details</h3>
+            <label for="phone_number">Phone Number:</label>
+            <input type="text" name="phone_number" id="phone_number" required>
+        </section>
+        <input type="submit" value="Place Order" class="checkout-btn">
+    </form>
+</section>
