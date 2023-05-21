55e33973b3fb808695fc62f37a7f4897fd72b95b
Nana <nana80414@outlook.com>
2023-05-21 20:19:10 +0100

    fix checkout bug

--- public/checkout.php
+++ public/checkout.php
@@ -10,5 +10,74 @@ if (!isset($_SESSION['login'])) {
     exit();
 }
 
-$output = loadTemplate('../templates/checkout.html.php', ['pricetag'=>$_POST['totalPrice']]);
+if (isset($_SESSION['bag'])){
+    $subtotal = 0;
+    for ($i=0; $i < count($_SESSION['bag']); $i++){
+        $bag[$i] = find($pdo, 'product', 'prodid', $_SESSION['bag'][$i]['prodid'])[0];
+        $bag[$i]['size'] = $_SESSION['bag'][$i]['size'];
+        $subtotal += $bag[$i]['prodprice'];
+    }
+
+    $templateVars= [
+        'bag' => $bag,
+        'subtotal' => $subtotal
+    ];
+}
+
+if(isset($_POST['submit'])){
+
+    if (isset($_POST['delivery_add_street']) && isset($_POST['delivery_add_county']) && isset($_POST['delivery_add_postcode']) && isset($_POST['delivery_add_country']) && isset($_POST['card_name']) && isset($_POST['card_number']) && isset($_POST['card_exp_date']) && isset($_POST['card_cvv']) && isset($_POST['phone_number'])) {
+        $delivery_add_street = $_POST['delivery_add_street'];
+        $delivery_add_county = $_POST['delivery_add_county'];
+        $delivery_add_postcode = $_POST['delivery_add_postcode'];
+        $delivery_add_country = $_POST['delivery_add_country'];
+        $card_name = $_POST['card_name'];
+        $card_number = $_POST['card_number'];
+        $card_exp_date = $_POST['card_exp_date'];
+        $card_cvv = $_POST['card_cvv'];
+        $phone_number = $_POST['phone_number'];
+    
+        $userid = $_SESSION['login'];
+    
+        $query = "INSERT INTO 'order' (prodid, delivery_add_street, delivery_add_county, delivery_add_postcode, delivery_add_country, price, quantity, card_name, card_number, card_exp_date, card_cvv, phone_number, billing_add_house_number, billing_add_street, billing_add_county, billing_add_postcode, billing_add_country, delivery_add_house_number, userid) VALUES ";
+    
+        foreach ($_SESSION['bag'] as $item) {
+            $prodid = $item['prodid'];
+            $price = $item['price'];
+            $quantity = $item['quantity'];
+            $billing_add_house_number = $item['billing_add_house_number'];
+            $billing_add_street = $item['billing_add_street'];
+            $billing_add_county = $item['billing_add_county'];
+            $billing_add_postcode = $item['billing_add_postcode'];
+            $billing_add_country = $item['billing_add_country'];
+            $delivery_add_house_number = $item['delivery_add_house_number'];
+    
+            $query .= "('$prodid', '$delivery_add_street', '$delivery_add_county', '$delivery_add_postcode', '$delivery_add_country', '$price', '$quantity', '$card_name', '$card_number', '$card_exp_date', '$card_cvv', '$phone_number', '$billing_add_house_number', '$billing_add_street', '$billing_add_county', '$billing_add_postcode', '$billing_add_country', '$delivery_add_house_number', '$userid'),";
+        }
+    
+        // Remove the trailing comma from the query
+        $query = rtrim($query, ',');
+    
+        $result = $pdo->query($query);
+    
+        if ($result) {
+            $_SESSION['bag'] = [];
+    
+            header('Location: order_confirmation.php');
+            exit();
+        } else {
+            // Error occurred while inserting the order details
+            echo "An error occurred while placing the order.";
+        }
+    }else{
+
+        $output = loadTemplate('../templates/checkout.html.php', $templateVars);
+
+    }
+}
+
+
+
+$output = loadTemplate('../templates/checkout.html.php', $templateVars);
+
 require '../templates/mainTemp.html.php';
--- templates/checkout.html.php
+++ templates/checkout.html.php
@@ -1,34 +1,4 @@
 <h2 class="checkout-title">Checkout</h2>
-<<<<<<< HEAD
-<section class="form-container">
-    <form action="order_confirmation.php" method="post" class="loginform">
-        <input type="hidden" name="totalPrice" value="<?=$pricetag?>">
-            <h3 class="section-title">Delivery Details</h3>
-            <label for="delivery_add_street">Street:</label>
-            <input type="text" name="delivery_add_street" id="delivery_add_street" required>
-            <label for="delivery_add_county">County:</label>
-            <input type="text" name="delivery_add_county" id="delivery_add_county" required>
-            <label for="delivery_add_postcode">Postcode:</label>
-            <input type="text" name="delivery_add_postcode" id="delivery_add_postcode" required>
-            <label for="delivery_add_country">Country:</label>
-            <input type="text" name="delivery_add_country" id="delivery_add_country" required>
-            <hr>
-            <h3 class="section-title">Payment Details</h3>
-            <label for="card_name">Cardholder Name:</label>
-            <input type="text" name="card_name" id="card_name" required>
-            <label for="card_number">Card Number:</label>
-            <input type="text" name="card_number" id="card_number" required>
-            <label for="card_exp_date">Card Expiration Date:</label>
-            <input type="text" name="card_exp_date" id="card_exp_date" required>
-            <label for="card_cvv">Card CVV:</label>
-            <input type="text" name="card_cvv" id="card_cvv" required>
-            <hr>
-            <h3 class="section-title">Contact Details</h3>
-            <label for="phone_number">Phone Number:</label>
-            <input type="text" name="phone_number" id="phone_number" required>
-        <input type="submit" value="Place Order" name="submit" class="checkout-btn">
-    </form>
-=======
 <section class="checkout-page">
     <section class="form-container">
         <form action="checkout.php" method="post" class="loginform">
@@ -81,5 +51,4 @@
             <h3>Total: £<?=$subtotal?></h3>
         </section>
     </section>
->>>>>>> 55fe1eec462d8f96d28a129da9b0bf102c3fddab
 </section>
